8b6b87514e1de0713321e064eb02202c75204563
xtron3r <aaronleal03@gmail.com>
2024-11-21 18:30:53 -0300

    #fix: navbar corrido en dispositivos moviles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 export default function Navbar(){ 
 
     return (
-        <nav className="">
-            <ul className="flex justify-center gap-4 py-4 text-white">
+        <nav>
+            <ul className="flex justify-center gap-4 pl-2 py-4 text-white">
                 <li><a href="#">Proyectos</a></li>
                 <li><a href="#">Experiencia</a></li>
-                <li><a href="#">Sobre Mí</a></li>
+                <li><a href="#" className="text-nowrap">Sobre Mí</a></li>
                 <li><a href="#">Contacto</a></li>
             </ul>
         </nav>

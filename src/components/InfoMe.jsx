98ad12dd5273e733d1bfd83108eafa4acd017598
xtron3r <aaronleal03@gmail.com>
2024-11-20 23:14:37 -0300

    Section navbar y hero

---
+++ src/components/InfoMe.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from 'typewriter-effect';
+
+export default function InfoMe() {
+    return (
+        <Typewriter
+        className="text-4xl font-bold text-green-500"
+        options={{
+            strings: 
+            [   '<span class="text-purple-600 text-4xl">Hola👋🏻, Soy Aron </span>',
+                '<span class="text-purple-600 text-4xl">Freelancer</span>',
+                '<span class="text-purple-600 text-4xl">Estudiante DuocUC</span>',
+            ],
+            loop: true,
+            delay: 100,
+            autoStart: true,
+            showCursor: true,
+            cursorBlink: true,
+            deleteSpeed:80,
+        }}/> 
+    )
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+
+export default function Navbar(){
+
+    return (
+        <nav className="">
+            <ul className="flex justify-center gap-5 py-5 text-white">
+                <li><a href="#">Proyectos</a></li>
+                <li><a href="#">Experiencia</a></li>
+                <li><a href="#">Sobre Mí</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    );
+}
